e87e2b36c1c756aad23a812e5b60570344755f3a
danil <danil369.ru@mail.ru>
2022-04-12 12:20:49 +0600

    Restaurant model

---
+++ helpers/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Base(models.Model):
+    created_date = models.DateField(auto_now=True)
+    updated_date = models.DateField(auto_now=True)
+    active = models.BooleanField(default=True)
+    name = models.CharField("", max_length=128)
+
+    def __str__(self):
+        return f"<{self.__class__.__name__}: {self.name}>"
+
+    class Meta:
+        abstract = True
+
+
+class Location(Base):
+    name = models.CharField('Адрес', max_length=120)
